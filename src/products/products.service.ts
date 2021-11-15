2a9c5d927f5e1079788fa26deb57189a47fd09a3
Chief-Aathil <aathilr7@gmail.com>
2021-11-15 11:47:36 +0530

    Service to update product.

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -4,24 +4,45 @@ import { Product } from './product.model';
 
 @Injectable()
 export class ProdutService {
-    private products: Product[] = [];
+  private products: Product[] = [];
 
-    insertProduct(title: string, desc: string, price: number) {
-        const prodId = new Date().toString()
-        const newProduct = new Product(prodId, title, desc, price);
-        this.products.push(newProduct);
-        return prodId;
-    }
+  insertProduct(title: string, desc: string, price: number) {
+    const prodId = this.products.length.toString();
+    const newProduct = new Product(prodId, title, desc, price);
+    this.products.push(newProduct);
+    return prodId;
+  }
 
-    getProducts() {
-        return [...this.products];
-    }
+  getProducts() {
+    return [...this.products];
+  }
 
-    getSingleProduct(productId: string) {
-        const product = this.products.find((prod) => prod.id === productId);
-        if (!product) {
-            throw new NotFoundException();
-        }
-        return { ...product };
+  getSingleProduct(productId: string) {
+    const product = this.findProduct(productId)[0];
+    return { ...product };
+  }
+
+  updateProduct(productId: string, title: string, desc: string, price: number) {
+    const [product, index] = this.findProduct(productId);
+    const updateProduct = { ...product };
+    if (title) {
+      updateProduct.title = title;
     }
+    if (desc) {
+      updateProduct.description = desc;
+    }
+    if (price) {
+      updateProduct.price = price;
+    }
+    this.products[index] = updateProduct;
+  }
+
+  private findProduct(id: string): [Product, number] {
+    const productIndex = this.products.findIndex((prod) => prod.id === id);
+    const product = this.products[productIndex];
+    if (!product) {
+      throw new NotFoundException('Could not find product.');
+    }
+    return [product, productIndex];
+  }
 }
